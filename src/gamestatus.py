2e473141bb9d6d4536b8d211e8702d592680f5e4
lottapispa <lotta.pispa@helsinki.fi>
2023-07-27 22:57:58 +0300

    testing

--- src/gamestatus.py
+++ src/gamestatus.py
@@ -3,25 +3,26 @@ class GameStatus:
     """Class that creates the gamerack and keeps track of the score."""
     def __init__(self):
         """Class constructor, creates variables."""
-        self.rows = 7
-        self.columns = 6
+        self.rows = 6
+        self.columns = 7
         # The rack is an empty matrix. Each list within the main list is a row.
-        self.rack = self.rack = [[0, 0, 0, 0, 0, 0, 0] for i in range(self.columns)]
+        self.rack = [[0, 0, 0, 0, 0, 0, 0] for i in range(self.rows)]
 
-    def insert_piece(self, row, column, color):
+    def insert_piece(self, row: int, column: int, color: str):
         """Inserts a piece to the rack."""
-        if color != "blue" or color != "red":
+        if color != "blue" and color != "red":
             raise ValueError("Wrong input, color needs to be blue or red!")
-        # if row is 1 the index is 0, and column 1's index is 0
-        if not 1 <= row <= 7:
+        if not 1 <= row <= 6:
             raise ValueError("Wrong input, rows are 1-7!")
-        if not 1 <= column <= 6:
+        if not 1 <= column <= 7:
             raise ValueError("Wrong input, columns are 1-6!")
+        # if row is 1 the index is 0, and column 1's index is 0
         # if there is already a piece in the index, you can't change it
         if self.rack[row-1][column-1] != 0:
             raise ValueError("Wrong input, that spot already has a piece!")
         else:
-            self.rack[row-1][column-1] = color
+            #self.rack[row-1][column-1] = color
+            self.rack[row-1].insert(column-1, color)
 
     def game_over(self):
         """Stops game if rack is full or a player connects four."""
@@ -34,4 +35,7 @@ class GameStatus:
 
 # for testing
 if __name__ == "__main__":
-    print(GameStatus().rack)
+    x = GameStatus()
+    print(x.rack)
+    x.insert_piece(1, 2, "blue")
+    print(x.rack)
--- src/tests/gamestatus_test.py
+++ src/tests/gamestatus_test.py
@@ -4,33 +4,49 @@ from gamestatus import GameStatus
 class TestGameStatus(unittest.TestCase):
     """Tests class gamestatus."""
     def setUp(self):
-        self.gamestatus = GameStatus()
-        self.rows = self.gamestatus.rows
-        self.columns = self.gamestatus.columns
-        self.rack = self.gamestatus.rack
+        self.rows = GameStatus().rows
+        self.columns = GameStatus().columns
 
     def test_correct_rack_in_the_beginning(self):
+        self.gamestatus = GameStatus()
+        self.rack = self.gamestatus.rack
         self.assertEqual(self.rack, [[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]])
 
     def test_insert_piece_wrong_input_color(self):
         """Tests if function raises value error for wrong input color and that the rack doesn't change."""
-        self.assertRaises(ValueError, self.gamestatus.insert_piece(2, 3, "b"))
+        self.gamestatus = GameStatus()
+        self.rack = self.gamestatus.rack
+        self.assertRaises(ValueError, self.gamestatus.insert_piece, 2, 3, "b")
         self.assertEqual(self.rack, [[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]])
 
     def test_insert_piece_wrong_input_row(self):
         """Tests if function raises value error for wrong input row and that the rack doesn't change."""
-        self.assertRaises(ValueError, self.gamestatus.insert_piece(8, 3, "blue"))
+        self.gamestatus = GameStatus()
+        self.rack = self.gamestatus.rack
+        self.assertRaises(ValueError, self.gamestatus.insert_piece, 7, 3, "blue")
         self.assertEqual(self.rack, [[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]])
 
     def test_insert_piece_wrong_input_column(self):
         """Tests if function raises value error for wrong input column and that the rack doesn't change."""
-        self.assertRaises(ValueError, self.gamestatus.insert_piece(5, 0, "blue"))
+        self.gamestatus = GameStatus()
+        self.rack = self.gamestatus.rack
+        self.assertRaises(ValueError, self.gamestatus.insert_piece, 5, 0, "blue")
         self.assertEqual(self.rack, [[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]])
 
     def test_insert_piece(self):
         """Tests if the rack changes correctly."""
-        self.rack = self.gamestatus.insert_piece(2, 3, "blue")
+        self.gamestatus = GameStatus()
+        self.rack = self.gamestatus.rack
+        self.gamestatus.insert_piece(2, 3, "blue")
         self.assertEqual(self.rack, [[0, 0, 0, 0, 0, 0, 0], [0, 0, "blue", 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]])
+
+    def test_insert_piece_wrong_input_spot_taken(self):
+        """Tests if function raises value error for wrong input (spot already taken) and that the rack doesn't change."""
+        self.gamestatus = GameStatus()
+        self.rack = self.gamestatus.rack
+        self.gamestatus.insert_piece(1, 2, "blue")
+        self.assertRaises(ValueError, self.gamestatus.insert_piece, 1, 2, "red")
+        self.assertEqual(self.rack, [[0, "blue", 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]])
 
     def game_over(self):
         pass
