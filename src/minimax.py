e1535bab5ac8cc53ac4a44745f6e5f5012e6fb7e
lottapispa <lotta.pispa@helsinki.fi>
2023-08-04 15:50:34 +0300

    coding main loop

--- src/minimax.py
+++ src/minimax.py
@@ -14,6 +14,7 @@ class Minimax:
         self.minimizer = Minimizer()
         self.commands = COMMANDS
         self.depth = 6
+        self.turn = None #who's turn is it, player 1 or 2
 
     def minimax(self, gamestatus, depth, a, b, maximizing_player: bool):
         if depth == 0 or gamestatus.is_game_over() is True:
@@ -23,7 +24,18 @@ class Minimax:
         pass
 
     def main(self):
-        self.minimax()
+        color = input("Choose color: 'red' or 'yellow'.")
+        who_starts = input("Do you want the first move? Type 'yes' or 'no'.")
+        if who_starts == "yes":
+            self.turn = 1
+            print("You are player 1.")
+        else:
+            self.turn = 2
+            print("You are player 2.")
+        while self.gamestatus.is_game_over() is False:
+            if self.turn == 1:
+                move = input("Player 1, make your move")
+        #self.minimax()
 
 # for testing
 if __name__ == "__main__":
