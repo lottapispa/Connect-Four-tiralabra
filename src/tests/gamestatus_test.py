7fc452d52cc0c9d55925ff69e2da5a5362496ed7
lottapispa <lotta.pispa@helsinki.fi>
2023-08-04 23:29:55 +0300

    fixing testing

--- src/tests/gamestatus_test.py
+++ src/tests/gamestatus_test.py
@@ -68,13 +68,11 @@ class TestGameStatus(unittest.TestCase):
 
     def test_is_game_over_tie(self):
         self.gamestatus = GameStatus()
-        self.rack = self.gamestatus.rack
-        self.over = self.gamestatus.over
         self.gamestatus.rack = [["yellow", "red", "yellow", "red", "yellow", "red", "yellow"], ["red", "yellow", "red", "yellow", "red", "yellow", "red"], ["red", "yellow", "red", "yellow", "red", "yellow", "red"], [
             "yellow", "red", "yellow", "red", "yellow", "red", "yellow"], ["yellow", "red", "yellow", "red", "yellow", "red", "yellow"], ["red", "yellow", "red", "yellow", "red", "yellow", "red"]]
         self.gamestatus.is_game_over()
-        self.assertTrue(self.over)
-        self.assertTrue(self.tie)
+        self.assertTrue(self.gamestatus.over)
+        self.assertTrue(self.gamestatus.tie)
 
     def test_is_game_over_false(self):
         self.gamestatus = GameStatus()
@@ -85,22 +83,16 @@ class TestGameStatus(unittest.TestCase):
 
     def test_check_for_win_hor(self):
         self.gamestatus = GameStatus()
-        self.gamestatus.rack, [[0, "red", "red", "red", "red", 0, 0], [0, "yellow", 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]
-        self.gamestatus.is_game_over()
-        self.assertEqual(self.gamestatus.over, True)
+        self.gamestatus.rack = [[0, "red", "red", "red", "red", 0, 0], [0, "yellow", 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]
+        self.assertTrue(self.gamestatus.check_for_win_hor())
 
     def test_check_for_win_ver(self):
         self.gamestatus = GameStatus()
-        self.rack = self.gamestatus.rack
-        self.over = self.gamestatus.over
-        self.rack, [[0, "red", 0, 0, 0, 0, 0], [0, "yellow", 0, 0, 0, 0, 0], [0, "yellow", 0, 0, 0, 0, 0], [0, "yellow", 0, 0, 0, 0, 0], [0, "yellow", 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]
-        self.gamestatus.is_game_over()
-        self.assertTrue(self.over)
+        self.gamestatus.rack = [[0, "red", 0, 0, 0, 0, 0], [0, "yellow", 0, 0, 0, 0, 0], [0, "yellow", 0, 0, 0, 0, 0], [0, "yellow", 0, 0, 0, 0, 0], [0, "yellow", 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]
+        self.assertTrue(self.gamestatus.check_for_win_ver())
 
     def test_check_for_win_dia(self):
         self.gamestatus = GameStatus()
-        self.rack = self.gamestatus.rack
-        self.over = self.gamestatus.over
-        self.rack, [[0, 0, 0, 0, 0, 0, 0], [0, 0, "red", 0, 0, 0, 0], [0, 0, 0, "red", 0, 0, 0], [0, 0, 0, 0, "red", 0, 0], [0, 0, 0, 0, 0, "red", 0], [0, 0, "yellow", 0, 0, 0, 0]]
-        self.gamestatus.is_game_over()
-        self.assertTrue(self.over)
+        self.gamestatus.rack = [[0, 0, 0, 0, 0, 0, 0], [0, 0, "red", 0, 0, 0, 0], [0, 0, 0, "red", 0, 0, 0], [0, 0, 0, 0, "red", 0, 0], [0, 0, 0, 0, 0, "red", 0], [0, 0, "yellow", 0, 0, 0, 0]]
+        self.assertTrue(self.gamestatus.check_for_win_dia())
+        
