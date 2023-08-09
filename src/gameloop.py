e5e18b679a17404898906c31adb0497ef2d0db47
lottapispa <lotta.pispa@helsinki.fi>
2023-08-09 19:21:38 +0300

    coding

--- src/gameloop.py
+++ src/gameloop.py
@@ -18,6 +18,14 @@ class GameLoop:
 
     def main(self):
         """Loop takes inputs from the user, starts the game and switches turns."""
+        self.players_color = input("Choose your pawns color: 'red' or 'yellow'. ")
+        if self.players_color == "red":
+            self.ai_color = "yellow"
+        elif self.players_color == "yellow":
+            self.ai_color = "red"
+        else:
+            raise ValueError("Wrong input, color needs to be red or yellow!")
+        
         self.who_starts = input(
             "Do you want the first move? Type 'yes' or 'no'. ")
         if self.who_starts == "yes":
@@ -26,13 +34,6 @@ class GameLoop:
             self.turn = False
         else:
             raise ValueError("Wrong input, answer needs to be yes or no!")
-        self.players_color = input("Choose color: 'red' or 'yellow'. ")
-        if self.players_color == "red":
-            self.ai_color = "yellow"
-        elif self.players_color == "yellow":
-            self.ai_color = "red"
-        else:
-            raise ValueError("Wrong input, color needs to be red or yellow!")
 
         while self.gamestatus.is_game_over() is False:
             if self.turn is True:
